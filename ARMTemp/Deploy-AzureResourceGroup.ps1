ff302899852b1053852264648368756a6e054ac0
Nalamala <VardhanNalamala@ToppanMerrillLLC.com>
2019-08-26 11:04:10 +0530

    ps1 added

---
+++ ARMTemp/Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,20 @@
+
+#Login into Azure using Azure service principle 
+
+$clientID = "4fba8a7c-f687-4c19-a175-ae3b4f985bcd"
+$key = "0qGYY:*XR_.95VN9Kkm+hG2M4QXF/A8p"
+$SecurePassword = $key | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $clientID, $SecurePassword
+$tenantID = "8a4817cb-3770-402d-a466-85c895620d10"
+Add-AzureRmAccount -Credential $cred -TenantId $tenantID -ServicePrincipal
+
+# Creating New ResourceGroup for Deployment
+
+New-AzureRmResourceGroup -Name DotNetApps -Location "Central US"
+
+#Deploying web app template to azure using template
+
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName DotNetApps `
+  -TemplateFile "C:\Program Files (x86)\Jenkins\workspace\Template_Pipeline\DotNetApp15\Template_Pipeline\WebSite.json" `
+  -TemplateParameterFile "C:\Program Files (x86)\Jenkins\workspace\Template_Pipeline\DotNetApp15\Template_Pipeline\WebSite.parameters.json"
+
